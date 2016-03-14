c4be91c193e832c60f4dea3eeed9d072cb37e720
gaoyiqun <ygao742@aucklanduni.ac.nz>
2016-03-14 22:08:42 +1300

    IntroduceUseOfSpecFlow

---
+++ SpecFlowIntro/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SpecFlowIntro
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; internal set; }
+       
+        public int SecondNumber { get; internal set; }
+
+        public void Add()
+        {
+            this.Result = this.FirstNumber + this.SecondNumber;
+        }
+
+        public int Result { get; internal set; }
+    }
+}
---
+++ SpecFlowIntro/CalculatorSteps.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowIntro
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        Calculator _calculator = new Calculator();
+
+        [Given(@"I have entered 50 into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator()
+        {
+            _calculator.FirstNumber = 50;
+        }
+        
+        [Given(@"I have also entered 70 into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator()
+        {
+            _calculator.SecondNumber = 70;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _calculator.Add();
+        }
+        
+        [Then(@"the result should be 120 on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen()
+        {
+            Assert.AreEqual(120, _calculator.Result);
+        }
+    }
+}
