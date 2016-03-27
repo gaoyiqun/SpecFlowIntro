70c91a762d28b78a64bd969f40947ba4e6c5ef80
gaoyiqun <ygao742@aucklanduni.ac.nz>
2016-03-27 20:31:01 +1300

    Using mock framework to establish Authentication unit testing. The testing is not depend on MyHash or Doc objects. DI allows testing to be complete by mocking Interfaces. The next step is to DI the Login and Authentication processes. Will create IAuthentication interface.

---
+++ WebBankSP.UnitTest/AuthenticationUnitTest.feature
@@ -0,0 +1,22 @@
+﻿@Authentication
+Feature: Authentication
+	In order to verify whether the input is valid
+	As a caller
+	I want to get the database and verify if the inputs are mathced with the sources.
+
+@mytag
+Scenario: Authentication successful with account number "1234" and password "Irene"
+	Given user account number is "1234"
+	And password is "Irene"
+	And IDoc will return "abc"
+	And IHash will return "abc"
+	When I call Verify method
+	Then the result should be "true"
+
+Scenario: Authentication unsuccessfulwith account number "1234" and password "Yiqun"
+	Given user account number is "1234"
+	And password is "Yiqun"
+	And IDoc will return "abc"
+	And IHash will return "bcd"
+	When I call Verify method
+	Then the result should be "false"
---
+++ WebBankSP.UnitTest/AuthenticationUnitTestSteps.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using System;
+using TechTalk.SpecFlow;
+
+namespace WebBankSP.UnitTest
+{
+    [Binding]
+    public class AuthenticationUnitTestSteps
+    {
+        private static Authentication target;
+        private static IHash hashStub;
+        private static IDoc docStub;
+
+        [BeforeScenario("Authentication")]
+        public static void BeforeScenarioAuthentication()
+        {
+            hashStub = MockRepository.GenerateStub<IHash>();
+            docStub = MockRepository.GenerateStub<IDoc>();
+
+            target = new Authentication(hashStub, docStub);
+            ScenarioContext.Current.Clear();
+        }
+
+        [Given(@"user account number is ""(.*)""")]
+        public void GivenUserAccountNumberIs(string id)
+        {
+            ScenarioContext.Current.Add("id", id);
+        }
+        
+        [Given(@"password is ""(.*)""")]
+        public void GivenPasswordIs(string password)
+        {
+            ScenarioContext.Current.Add("password", password);
+        }
+        
+        [Given(@"IDoc will return ""(.*)""")]
+        public void GivenIDocWillReturn(string docPassword)
+        {
+            docStub.Stub(x=> x.GetPassword(Arg<string>.Is.Anything)).Return(docPassword);
+        }
+        
+        [Given(@"IHash will return ""(.*)""")]
+        public void GivenIHashWillReturn(string hashPassword)
+        {
+            hashStub.Stub(x => x.GetHash(Arg<string>.Is.Anything)).Return(hashPassword);
+
+        }
+
+        [When(@"I call Verify method")]
+        public void WhenICallVerifyMethod()
+        {
+            var id = ScenarioContext.Current["id"].ToString();
+            var password = ScenarioContext.Current["password"].ToString();
+            var result = target.Verify(id,password);
+
+            ScenarioContext.Current.Add("result", result);
+
+        }
+        
+        [Then(@"the result should be ""(.*)""")]
+        public void ThenTheResultShouldBe(string expected)
+        {
+            var isValid = Convert.ToBoolean(expected);
+            var actual = Convert.ToBoolean(ScenarioContext.Current["result"]);
+
+            Assert.AreEqual(isValid, actual);
+        }
+    }
+}
